c92379bd0ef07d1b80c6e5891354990bbf933527
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2025-11-15 16:23:54 +0700

    update admin, login for session2

--- saleappv2/eapp/admin.py
+++ saleappv2/eapp/admin.py
@@ -1,9 +1,39 @@
+from tkinter.font import names
+
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from eapp.models import Category, Product
-
+from eapp.models import Category, Product, UserRole
 from eapp import db, app
+from flask_login import current_user, logout_user
+from flask_admin import BaseView, expose
+from flask import redirect
+
+class AdminView(ModelView):
+    def is_accessible(self) -> bool:
+        return current_user.is_authenticated and current_user.user_role == UserRole.ADMIN
+
+class ProductView(AdminView):
+    column_list = ['id', 'name', 'price', 'active', 'category_id']
+    column_searchable_list = ['name']
+    column_filters = ['id', 'name', 'price']
+    can_export = True
+    edit_modal = True
+    column_editable_list = ['name']
+    page_size = 30
+
+
+class LogoutView(BaseView):
+    @expose('/')
+    def index(self):
+        logout_user()
+        return redirect('/admin')
+
+    def is_accessible(self) -> bool:
+        return current_user.is_authenticated
+
 
 admin = Admin(app=app, name="e-Commerce's Admin")
-admin.add_view(ModelView(Category, db.session))
-admin.add_view(ModelView(Product, db.session))
+
+admin.add_view(AdminView(Category, db.session))
+admin.add_view(ProductView(Product, db.session))
+admin.add_view(LogoutView(name='Đăng xuất'))
--- saleappv2/eapp/dao.py
+++ saleappv2/eapp/dao.py
@@ -1,4 +1,6 @@
-from eapp.models import Category, Product
+from eapp.models import Category, Product, User
+import hashlib
+from eapp import app
 
 
 def load_categories():
@@ -13,4 +15,21 @@ def load_products(cate_id=None, kw=None, page=1):
     if cate_id:
         query = query.filter(Product.category_id.__eq__(cate_id))
 
+    if page:
+        start = (page - 1) * app.config['PAGE_SIZE']
+        query = query.slice(start, start + app.config['PAGE_SIZE'])
+
     return query.all()
+
+
+def count_products():
+    return Product.query.count()
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    return User.query.filter(User.username==username,
+                             User.password==password).first()
